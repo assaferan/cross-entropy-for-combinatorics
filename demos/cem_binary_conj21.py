4902f4794e602e1c0df0205cdf55e235c1b10c8e
Eran Assaf <assaferan@gmail.com>
2024-09-09 18:20:20 -0400

    updated gitignore and parametrized some of the functions. Added a GMM regression to learn the distribution.

--- demos/cem_binary_conj21.py
+++ demos/cem_binary_conj21.py
@@ -28,54 +28,109 @@ import pickle
 import time
 import math
 import matplotlib.pyplot as plt
+from sklearn import mixture
+from matplotlib.colors import LogNorm
 
+def initModel(N):
+        # N = 19   #number of vertices in the graph. Only used in the reward function, not directly relevant to the algorithm 
+        MYN = int(N*(N-1)/2)  #The length of the word we are generating. Here we are generating a graph, so we create a 0-1 word of length (N choose 2)
 
-N = 19   #number of vertices in the graph. Only used in the reward function, not directly relevant to the algorithm 
-MYN = int(N*(N-1)/2)  #The length of the word we are generating. Here we are generating a graph, so we create a 0-1 word of length (N choose 2)
+        LEARNING_RATE = 0.0001 #Increase this to make convergence faster, decrease if the algorithm gets stuck in local optima too often.
+        n_sessions =1000 #number of new sessions per iteration
+        percentile = 93 #top 100-X percentile we are learning from
+        super_percentile = 94 #top 100-X percentile that survives to next iteration
 
-LEARNING_RATE = 0.0001 #Increase this to make convergence faster, decrease if the algorithm gets stuck in local optima too often.
-n_sessions =1000 #number of new sessions per iteration
-percentile = 93 #top 100-X percentile we are learning from
-super_percentile = 94 #top 100-X percentile that survives to next iteration
+        FIRST_LAYER_NEURONS = 128 #Number of neurons in the hidden layers.
+        SECOND_LAYER_NEURONS = 64
+        THIRD_LAYER_NEURONS = 4
 
-FIRST_LAYER_NEURONS = 128 #Number of neurons in the hidden layers.
-SECOND_LAYER_NEURONS = 64
-THIRD_LAYER_NEURONS = 4
-
-n_actions = 2 #The size of the alphabet. In this file we will assume this is 2. There are a few things we need to change when the alphabet size is larger,
+        n_actions = 2 #The size of the alphabet. In this file we will assume this is 2. There are a few things we need to change when the alphabet size is larger,
 			  #such as one-hot encoding the input, and using categorical_crossentropy as a loss function.
 			  
-observation_space = 2*MYN #Leave this at 2*MYN. The input vector will have size 2*MYN, where the first MYN letters encode our partial word (with zeros on
-						  #the positions we haven't considered yet), and the next MYN bits one-hot encode which letter we are considering now.
-						  #So e.g. [0,1,0,0,   0,0,1,0] means we have the partial word 01 and we are considering the third letter now.
-						  #Is there a better way to format the input to make it easier for the neural network to understand things?
+        observation_space = 2*MYN #Leave this at 2*MYN. The input vector will have size 2*MYN, where the first MYN letters encode our partial word (with zeros on
+	#the positions we haven't considered yet), and the next MYN bits one-hot encode which letter we are considering now.
+	#So e.g. [0,1,0,0,   0,0,1,0] means we have the partial word 01 and we are considering the third letter now.
+	#Is there a better way to format the input to make it easier for the neural network to understand things?
+				  
+        len_game = MYN 
+        state_dim = (observation_space,)
+
+        INF = 1000000
 
 
-						  
-len_game = MYN 
-state_dim = (observation_space,)
+        #Model structure: a sequential network with three hidden layers, sigmoid activation in the output.
+        #I usually used relu activation in the hidden layers but play around to see what activation function and what optimizer works best.
+        #It is important that the loss is binary cross-entropy if alphabet size is 2.
 
-INF = 1000000
+        model = Sequential()
+        model.add(Dense(FIRST_LAYER_NEURONS,  activation="relu"))
+        model.add(Dense(SECOND_LAYER_NEURONS, activation="relu"))
+        model.add(Dense(THIRD_LAYER_NEURONS, activation="relu"))
+        model.add(Dense(1, activation="sigmoid"))
+        model.build((None, observation_space))
+        model.compile(loss="binary_crossentropy", optimizer=SGD(learning_rate = LEARNING_RATE)) #Adam optimizer also works well, with lower learning rate
 
-
-#Model structure: a sequential network with three hidden layers, sigmoid activation in the output.
-#I usually used relu activation in the hidden layers but play around to see what activation function and what optimizer works best.
-#It is important that the loss is binary cross-entropy if alphabet size is 2.
-
-model = Sequential()
-model.add(Dense(FIRST_LAYER_NEURONS,  activation="relu"))
-model.add(Dense(SECOND_LAYER_NEURONS, activation="relu"))
-model.add(Dense(THIRD_LAYER_NEURONS, activation="relu"))
-model.add(Dense(1, activation="sigmoid"))
-model.build((None, observation_space))
-model.compile(loss="binary_crossentropy", optimizer=SGD(learning_rate = LEARNING_RATE)) #Adam optimizer also works well, with lower learning rate
-
-print(model.summary())
+        print(model.summary())
+        return model, len_game, observation_space, n_sessions, MYN, INF, percentile, super_percentile
 
 
+def stateToGraph(state):
+    #Construct the graph
+    N = int((1+np.sqrt(1+8*len(state)))/2)
+    G= nx.Graph()
+    G.add_nodes_from(list(range(N)))
+    count = 0
+    for i in range(N):
+        for j in range(i+1,N):
+            if state[count] == 1:
+                G.add_edge(i,j)
+            count += 1
+    return G
 
+def adjacencyMatrix(state):
+    N = int((1+np.sqrt(1+8*len(state)))/2)
+    mat = np.zeros((N,N))
+    count = 0
+    for i in range(N):
+        for j in range(i+1,N):
+            mat[i,j] = state[count]
+            mat[j,i] = mat[i,j]
+            count += 1
+    return mat
 
-def calcScore(state):
+def matrixToData(mat):
+    N = mat.shape[0]
+    data = []
+    for i in range(N):
+        for j in range(N):
+            if mat[i][j] == 1:
+                pt = ((2*i+1)/(2*N), (2*j+1)/(2*N))
+                data.append(pt)
+    return np.array(data)
+
+def showGMM(clf, X_train):
+    x = np.linspace(0.0, 1.0)
+    y = np.linspace(0.0, 1.0)
+    X, Y = np.meshgrid(x, y)
+    XX = np.array([X.ravel(), Y.ravel()]).T
+    Z = -clf.score_samples(XX)
+    Z = Z.reshape(X.shape)
+
+    CS = plt.contour(
+            X, Y, Z, norm=LogNorm(vmin=1.0, vmax=1000.0), levels=np.logspace(0, 3, 10)
+    )
+    CB = plt.colorbar(CS, shrink=0.8, extend="both")
+    plt.scatter(X_train[:, 0], X_train[:, 1], 0.8)
+
+    plt.title("Negative log-likelihood predicted by a GMM")
+    plt.axis("tight")
+    plt.show()
+    return
+
+# loading from a string
+# a = np.fromstring('0 0 0 0 0 0 1 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0 0 1 1 0 0 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0', dtype=int, sep = ' ')
+
+def calcScore(state, N, INF):
 	"""
 	Calculates the reward for a given word. 
 	This function is very slow, it can be massively sped up with numba -- but numba doesn't support networkx yet, which is very convenient to use here
@@ -120,11 +175,12 @@ def calcScore(state):
 	#We add to this the conjectured best value. This way if the reward is positive we know we have a counterexample.
 	myScore = math.sqrt(N-1) + 1 - lambda1 - mu
 	if myScore > 0:
-		#You have found a counterexample. Do something with it.
-		print(state)
-		nx.draw_kamada_kawai(G)
-		plt.show()
-		exit()
+	    #You have found a counterexample. Do something with it.
+            print(state)
+            print(myScore)
+            nx.draw_kamada_kawai(G)
+            plt.show()
+            exit()
 		
 	return myScore
 
@@ -135,7 +191,7 @@ def calcScore(state):
 	
 						
 
-def generate_session(agent, n_sessions, verbose = 1):
+def generate_session(agent, n_sessions, len_game, observation_space, MYN, N, INF, verbose = 1):
 	"""
 	Play n_session games using agent neural network.
 	Terminate when games finish 
@@ -177,7 +233,7 @@ def generate_session(agent, n_sessions, verbose = 1):
 			terminal = step == MYN
 			tic = time.time()
 			if terminal:
-				total_score[i] = calcScore(state_next[i])
+				total_score[i] = calcScore(state_next[i], N, INF)
 			scorecalc_time += time.time()-tic
 			tic = time.time()
 			if not terminal:
@@ -194,7 +250,7 @@ def generate_session(agent, n_sessions, verbose = 1):
 
 
 
-def select_elites(states_batch, actions_batch, rewards_batch, percentile=50):
+def select_elites(states_batch, actions_batch, rewards_batch, n_sessions, percentile=50):
 	"""
 	Select states and actions from games that have rewards >= percentile
 	:param states_batch: list of lists of states, states_batch[session_i][t]
@@ -224,7 +280,7 @@ def select_elites(states_batch, actions_batch, rewards_batch, percentile=50):
 	elite_actions = np.array(elite_actions, dtype = int)	
 	return elite_states, elite_actions
 	
-def select_super_sessions(states_batch, actions_batch, rewards_batch, percentile=90):
+def select_super_sessions(states_batch, actions_batch, rewards_batch, n_sessions, percentile=90):
 	"""
 	Select all the sessions that will survive to the next generation
 	Similar to select_elites function
@@ -248,91 +304,113 @@ def select_super_sessions(states_batch, actions_batch, rewards_batch, percentile
 	super_actions = np.array(super_actions, dtype = int)
 	super_rewards = np.array(super_rewards)
 	return super_states, super_actions, super_rewards
+
+# Originally we had N=19 and num_generations = 10**6.
+# Want to figure out how num_generations should depend on N ?
+
+def runML(N, num_generations):
+
+        model, len_game, observation_space, n_sessions, MYN, INF, percentile, super_percentile = initModel(N)
+        
+        super_states =  np.empty((0,len_game,observation_space), dtype = int)
+        super_actions = np.array([], dtype = int)
+        super_rewards = np.array([])
+        sessgen_time = 0
+        fit_time = 0
+        score_time = 0
+
+        myRand = random.randint(0,1000) #used in the filename
+
+        for i in range(num_generations): #1000000 generations should be plenty
+	        #generate new sessions
+	        #performance can be improved with joblib
+	        tic = time.time()
+	        sessions = generate_session(model,n_sessions,len_game, observation_space, MYN, N, INF, 0) #change 0 to 1 to print out how much time each step in generate_session takes 
+	        sessgen_time = time.time()-tic
+	        tic = time.time()
 	
-
-super_states =  np.empty((0,len_game,observation_space), dtype = int)
-super_actions = np.array([], dtype = int)
-super_rewards = np.array([])
-sessgen_time = 0
-fit_time = 0
-score_time = 0
-
-
-
-myRand = random.randint(0,1000) #used in the filename
-
-for i in range(1000000): #1000000 generations should be plenty
-	#generate new sessions
-	#performance can be improved with joblib
-	tic = time.time()
-	sessions = generate_session(model,n_sessions,0) #change 0 to 1 to print out how much time each step in generate_session takes 
-	sessgen_time = time.time()-tic
-	tic = time.time()
+	        states_batch = np.array(sessions[0], dtype = int)
+	        actions_batch = np.array(sessions[1], dtype = int)
+	        rewards_batch = np.array(sessions[2])
+	        states_batch = np.transpose(states_batch,axes=[0,2,1])
 	
-	states_batch = np.array(sessions[0], dtype = int)
-	actions_batch = np.array(sessions[1], dtype = int)
-	rewards_batch = np.array(sessions[2])
-	states_batch = np.transpose(states_batch,axes=[0,2,1])
-	
-	states_batch = np.append(states_batch,super_states,axis=0)
+	        states_batch = np.append(states_batch,super_states,axis=0)
 
-	if i>0:
-		actions_batch = np.append(actions_batch,np.array(super_actions),axis=0)	
-	rewards_batch = np.append(rewards_batch,super_rewards)
+	        if i>0:
+		        actions_batch = np.append(actions_batch,np.array(super_actions),axis=0)	
+	        rewards_batch = np.append(rewards_batch,super_rewards)
 		
-	randomcomp_time = time.time()-tic 
-	tic = time.time()
+	        randomcomp_time = time.time()-tic 
+	        tic = time.time()
 
-	elite_states, elite_actions = select_elites(states_batch, actions_batch, rewards_batch, percentile=percentile) #pick the sessions to learn from
-	select1_time = time.time()-tic
+	        elite_states, elite_actions = select_elites(states_batch, actions_batch, rewards_batch, n_sessions, percentile=percentile) #pick the sessions to learn from
+	        select1_time = time.time()-tic
 
-	tic = time.time()
-	super_sessions = select_super_sessions(states_batch, actions_batch, rewards_batch, percentile=super_percentile) #pick the sessions to survive
-	select2_time = time.time()-tic
+	        tic = time.time()
+	        super_sessions = select_super_sessions(states_batch, actions_batch, rewards_batch, n_sessions, percentile=super_percentile) #pick the sessions to survive
+	        select2_time = time.time()-tic
 	
-	tic = time.time()
-	super_sessions = [(super_sessions[0][i], super_sessions[1][i], super_sessions[2][i]) for i in range(len(super_sessions[2]))]
-	super_sessions.sort(key=lambda super_sessions: super_sessions[2],reverse=True)
-	select3_time = time.time()-tic
+	        tic = time.time()
+	        super_sessions = [(super_sessions[0][i], super_sessions[1][i], super_sessions[2][i]) for i in range(len(super_sessions[2]))]
+	        super_sessions.sort(key=lambda super_sessions: super_sessions[2],reverse=True)
+	        select3_time = time.time()-tic
 	
-	tic = time.time()
-	model.fit(elite_states, elite_actions) #learn from the elite sessions
-	fit_time = time.time()-tic
+	        tic = time.time()
+	        model.fit(elite_states, elite_actions) #learn from the elite sessions
+	        fit_time = time.time()-tic
 	
-	tic = time.time()
+	        tic = time.time()
 	
-	super_states = [super_sessions[i][0] for i in range(len(super_sessions))]
-	super_actions = [super_sessions[i][1] for i in range(len(super_sessions))]
-	super_rewards = [super_sessions[i][2] for i in range(len(super_sessions))]
+	        super_states = [super_sessions[i][0] for i in range(len(super_sessions))]
+	        super_actions = [super_sessions[i][1] for i in range(len(super_sessions))]
+	        super_rewards = [super_sessions[i][2] for i in range(len(super_sessions))]
 	
-	rewards_batch.sort()
-	mean_all_reward = np.mean(rewards_batch[-100:])	
-	mean_best_reward = np.mean(super_rewards)	
+	        rewards_batch.sort()
+	        mean_all_reward = np.mean(rewards_batch[-100:])	
+	        mean_best_reward = np.mean(super_rewards)	
 
-	score_time = time.time()-tic
+	        score_time = time.time()-tic
 	
-	print("\n" + str(i) +  ". Best individuals: " + str(np.flip(np.sort(super_rewards))))
+	        print("\n" + str(i) +  ". Best individuals: " + str(np.flip(np.sort(super_rewards))))
 	
-	#uncomment below line to print out how much time each step in this loop takes. 
-	print(	"Mean reward: " + str(mean_all_reward) + "\nSessgen: " + str(sessgen_time) + ", other: " + str(randomcomp_time) + ", select1: " + str(select1_time) + ", select2: " + str(select2_time) + ", select3: " + str(select3_time) +  ", fit: " + str(fit_time) + ", score: " + str(score_time)) 
+	        #uncomment below line to print out how much time each step in this loop takes. 
+	        print(	"Mean reward: " + str(mean_all_reward) + "\nSessgen: " + str(sessgen_time) + ", other: " + str(randomcomp_time) + ", select1: " + str(select1_time) + ", select2: " + str(select2_time) + ", select3: " + str(select3_time) +  ", fit: " + str(fit_time) + ", score: " + str(score_time)) 
 	
-	
-	if (i%20 == 1): #Write all important info to files every 20 iterations
-		with open('best_species_pickle_'+str(myRand)+'.txt', 'wb') as fp:
-			pickle.dump(super_actions, fp)
-		with open('best_species_txt_'+str(myRand)+'.txt', 'w') as f:
-			for item in super_actions:
-				f.write(str(item))
-				f.write("\n")
-		with open('best_species_rewards_'+str(myRand)+'.txt', 'w') as f:
-			for item in super_rewards:
-				f.write(str(item))
-				f.write("\n")
-		with open('best_100_rewards_'+str(myRand)+'.txt', 'a') as f:
-			f.write(str(mean_all_reward)+"\n")
-		with open('best_elite_rewards_'+str(myRand)+'.txt', 'a') as f:
-			f.write(str(mean_best_reward)+"\n")
-	if (i%200==2): # To create a timeline, like in Figure 3
-		with open('best_species_timeline_txt_'+str(myRand)+'.txt', 'a') as f:
-			f.write(str(super_actions[0]))
-			f.write("\n")
+	        fname_suffix = str(N)+'_'+str(num_generations) + '_' + str(myRand)
+	        if (i%20 == 1): #Write all important info to files every 20 iterations
+		        with open('best_species_pickle_'+fname_suffix+'.txt', 'wb') as fp:
+			        pickle.dump(super_actions, fp)
+		        with open('best_species_txt_'+fname_suffix+'.txt', 'w') as f:
+			        for item in super_actions:
+				        f.write(str(item))
+				        f.write("\n")
+		        with open('best_species_rewards_'+fname_suffix+'.txt', 'w') as f:
+			        for item in super_rewards:
+				        f.write(str(item))
+				        f.write("\n")
+		        with open('best_100_rewards_'+fname_suffix+'.txt', 'a') as f:
+			        f.write(str(mean_all_reward)+"\n")
+		        with open('best_elite_rewards_'+fname_suffix+'.txt', 'a') as f:
+			        f.write(str(mean_best_reward)+"\n")
+	        if (i%200==2): # To create a timeline, like in Figure 3
+		        with open('best_species_timeline_txt_'+fname_suffix+'.txt', 'a') as f:
+			        f.write(str(super_actions[0]))
+			        f.write("\n")
+        return super_actions # [stateToGraph(specie) for specie in super_actions]
+
+def learnPDF():
+    min_N = 4
+    max_N = 10
+    n_samples = 300
+
+    species = []
+    for i in range(n_samples):
+        N = np.random.randint(min_N, max_N)
+        num_gens = int(10**(N/3)) # a guess
+        species += runML(N, num_gens)
+
+    X_train_raw = [adjacencyMatrix(state) for state in species]
+    X_train = np.vstack([matrixToData(raw) for raw in X_train_raw])
+    clf = mixture.GaussianMixture(n_components=2, covariance_type="full")
+    clf.fit(X_train)
+    return clf, X_train
